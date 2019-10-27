8d9733223385b1733c851b228232dc31bc9f44e1
KrupoderovMikhail <mishka.crash@gmail.com>
2019-10-27 13:41:30 +0500

    GameLoop.

--- src/com/krupoderov/game/GamePanel.java
+++ src/com/krupoderov/game/GamePanel.java
@@ -47,23 +47,82 @@ public class GamePanel extends JPanel implements Runnable {
     public void run() {
         init();
 
+        final double GAME_HERTZ = 60.0;
+        final double TBU = 1_000_000_000 / GAME_HERTZ; // Time before update
+
+        final int MUBR = 5; // Must update before render
+
+        double lastUpdateTime = System.nanoTime();
+        double lastRenderTime;
+
+        final double TARGER_FPS = 60;
+        final double TTBR = 1_000_000_000 / TARGER_FPS; // Total time before render
+
+        int frameCount = 0;
+        int lastSecondTime = (int) (lastUpdateTime / 1_000_000_000);
+        int oldFrameCount = 0;
+
         while (running) {
-            update();
+            double now = System.nanoTime();
+            int updateCount = 0;
+            while (((now - lastUpdateTime) > TBU) && (updateCount < MUBR)) {
+                update();
+                input();
+                lastUpdateTime += TBU;
+                updateCount++;
+            }
+
+            if (now - lastUpdateTime > TBU) {
+                lastUpdateTime = now - TBU;
+            }
+
+            input();
             render();
             draw();
+            lastRenderTime = now;
+            frameCount++;
+
+            int thisSecond = (int) (lastUpdateTime / 1_000_000_000);
+            if (thisSecond > lastSecondTime) {
+                if (frameCount != oldFrameCount) {
+                    System.out.println("NEW SECOND " + thisSecond + " " + frameCount);
+                    oldFrameCount = frameCount;
+                }
+                frameCount = 0;
+                lastSecondTime = thisSecond;
+
+                while (now - lastRenderTime < TTBR && now - lastUpdateTime < TBU) {
+                    Thread.yield();
+
+                    try {
+                        Thread.sleep(1);
+                    }catch (Exception e) {
+                        System.out.println("ERROR: yielding thread");
+                    }
+
+                    now = System.nanoTime();
+                }
+            }
         }
     }
-
-    private int x = 0;
 
     public void update() {
     }
 
-    public void render() {
+    public void input(){
 
     }
 
-    public void draw() {
+    public void render() {
+        if (graphics2D != null) {
+            graphics2D.setColor(new Color(40, 132, 244));
+            graphics2D.fillRect(0, 0, width, height);
+        }
+    }
 
+    public void draw() {
+        Graphics graphics = (Graphics) this.getGraphics();
+        graphics.drawImage(image, 0, 0, width, height, null);
+        graphics.dispose();
     }
 }
