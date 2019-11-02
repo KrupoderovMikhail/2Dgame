16a972fc98311bc2b14b88f0e90102224c646df8
KrupoderovMikhail <mishka.crash@gmail.com>
2019-11-02 19:28:24 +0500

    Creating the TileManager class and TileMap class.

--- src/com/krupoderov/game/states/PlayState.java
+++ src/com/krupoderov/game/states/PlayState.java
@@ -4,6 +4,7 @@ import com.krupoderov.game.GamePanel;
 import com.krupoderov.game.entity.Player;
 import com.krupoderov.game.graphics.Font;
 import com.krupoderov.game.graphics.Sprite;
+import com.krupoderov.game.tiles.TileManager;
 import com.krupoderov.game.util.KeyHandler;
 import com.krupoderov.game.util.MouseHandler;
 import com.krupoderov.game.util.Vector2f;
@@ -17,11 +18,13 @@ public class PlayState extends GameState {
 
     private Font font;
     private Player player;
+    private TileManager tm;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
+        tm = new TileManager("tile/tilemap.xml");
         font = new Font("font/font.png", 10, 10);
-        player = new Player(new Sprite("entity/linkFormatted.png"), new Vector2f(300, 300), 128); // size 128
+        player = new Player(new Sprite("entity/linkFormatted.png"), new Vector2f(300, 300), 128);
     }
 
     @Override
@@ -33,7 +36,7 @@ public class PlayState extends GameState {
     public void input(MouseHandler mouse, KeyHandler key) {
         player.input(mouse, key);
     }
-    
+
     @Override
     public void render(Graphics2D g) {
         Sprite.drawArray(g, font, GamePanel.oldFrameCount + " FPS", new Vector2f(GamePanel.width - 192, 32), 32, 32, 24, 0);
---
+++ src/com/krupoderov/game/tiles/TileMap.java
@@ -0,0 +1,10 @@
+package com.krupoderov.game.tiles;
+
+import java.awt.*;
+
+/**
+ * Created by Krupoderov Mikhail on Nov, 2019
+ */
+public abstract class TileMap {
+    public abstract void render(Graphics2D g);
+}
